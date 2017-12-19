a89687b41543bbae949a2aa7a3327bcc17fefa60
Casey <cb@shotgunflat.com>
2017-12-18 22:35:25 -0500

    Dialogue work

--- Assets/scripts/alium.cs
+++ Assets/scripts/alium.cs
@@ -17,12 +17,16 @@ public class alium : MonoBehaviour {
     flower myFlower;
     friend myFriend;
 
+    public bool talking;
+
 	void Start() {
 		lover = GetComponent<CharacterController> ();
 
 		Cursor.lockState = CursorLockMode.Locked;
 		Cursor.visible = false;
-	}
+
+        talking = false;
+    }
 
     void Update() {
         // lover movement
@@ -36,51 +40,59 @@ public class alium : MonoBehaviour {
         // lover rotation
         // float rot = Input.GetAxis("Mouse X") * rotSens;
 
-        if (movX != 0) {
-            currentDir = Mathf.Sign(movX);
-            if (lastDir != currentDir) {
-                transform.Rotate(0, 180f, 0);
-                lastDir = currentDir;
-                if (myFlower) {
-                    myFlower.Flip();
+        if (talking == false) { 
+            if (movX != 0) {
+                currentDir = Mathf.Sign(movX);
+                if (lastDir != currentDir) {
+                    transform.Rotate(0, 180f, 0);
+                    lastDir = currentDir;
+                    if (myFlower) {
+                        myFlower.Flip();
+                    }
                 }
             }
+            lover.Move(mov);
         }
 
-        lover.Move(mov);
         // transform.Rotate (0, 0, -rot);
 
         // wind time
         if (Input.GetMouseButtonDown(0)) {
-            //testSprite = Instantiate (testSpritePrefab) as GameObject;
-            //testSprite.transform.position = transform.TransformPoint (0, .1f, 0);
-            //testSprite.transform.rotation = transform.rotation;
-            RaycastHit2D hit = Physics2D.CircleCast(transform.position, 2.0f, transform.up, 2.0f);
-            if (hit.collider) {
-                myFriend = hit.transform.gameObject.GetComponent<friend>();
-				if (myFlower == null) {
-					myFlower = hit.transform.gameObject.GetComponent<flower> ();
-				}
-                // pass position and rotation to potential so it knows where to fly away from
-                if (myFlower) {
-                    myFlower.alium = this.gameObject;
-                    myFlower.newPos();
-                }
-                if (myFriend) {
-                    if (myFlower && myFlower.attached){
-                        Debug.Log("i love you!");
-						Destroy (myFlower.gameObject);
-						myFriend.GotGift ();
-						myFriend = null;
-					} else {
-						myFriend.Hey ();
-						myFriend = null;
+            if (talking == false) { 
+                //testSprite = Instantiate (testSpritePrefab) as GameObject;
+                //testSprite.transform.position = transform.TransformPoint (0, .1f, 0);
+                //testSprite.transform.rotation = transform.rotation;
+                RaycastHit2D hit = Physics2D.CircleCast(transform.position, 2.0f, transform.up, 2.0f);
+                if (hit.collider) {
+                    myFriend = hit.transform.gameObject.GetComponent<friend>();
+			    	if (myFlower == null) {
+			    		myFlower = hit.transform.gameObject.GetComponent<flower> ();
+			    	}
+                    // pass position and rotation to potential so it knows where to fly away from
+                    if (myFlower) {
+                        myFlower.alium = this.gameObject;
+                        myFlower.newPos();
+                    }
+                    if (myFriend) {
+                        talking = true;
+                        if (myFlower && myFlower.attached){
+                            Debug.Log("i love you!");
+			    			Destroy (myFlower.gameObject);
+			    			myFriend.GotGift ();
+			    		} else {
+			    			myFriend.Hey ();
+                        }
                     }
                 }
+            } else {
+                myFriend.Bye();
+                talking = false;
+                myFriend = null;
             }
         }
 
-        if (Input.GetMouseButtonDown(1)) {
+
+        if (Input.GetMouseButtonDown(1) && talking == false) {
             if (myFlower) { 
                 myFlower.attached = false;
 				myFlower.Drop ();
--- Assets/scripts/friend.cs
+++ Assets/scripts/friend.cs
@@ -5,9 +5,12 @@ using UnityEngine.UI;
 
 public class friend : MonoBehaviour {
 
+    [SerializeField] GameObject alien;
+
 	public int gifts = 0;
 
 	private string def = "Hey! This is what I say by default!";
+    private string thanks = "Wow! I love you!";
 	private string curMes;
 
 	private float rot = 0;
@@ -22,41 +25,63 @@ public class friend : MonoBehaviour {
 	private float lastTime;
 	private float currentTime;
 
+    private bool talkedTo;
+
 	[SerializeField] Canvas speechCanvas;
 	[SerializeField] Text speech;
 
 	void Start() {
 		curMes = def;
 
+        talkedTo = false;
+
 		rot = Random.Range(0, 360);
 		delta = 0;
 	}
 
-	void Update () {
-		transform.Translate (0, speed * Time.deltaTime, 0);
-		transform.localEulerAngles = new Vector3 (0, 0, rot);
-		currentTime = Time.fixedTime - lastTime;
-		if (currentTime >= delta) {
-			speed = 0;
-		}
-		if (currentTime >= delta + wait) {
-			ChangeDirection ();
-		}
+    void Update() {
+        if (talkedTo == false) { 
+		    transform.Translate (0, speed * Time.deltaTime, 0);
+		    transform.localEulerAngles = new Vector3 (0, 0, rot);
+		    currentTime = Time.fixedTime - lastTime;
+		    if (currentTime >= delta) {
+		    	speed = 0;
+		    }
+		    if (currentTime >= delta + wait) {
+		    	ChangeDirection ();
+		    }
+        } else {
+
+        }
 	}
 
 	public void Hey() {
+        talkedTo = true;
 		speech.text = curMes;
 		speechCanvas.enabled = true;
 	}
+
+    public void Bye() {
+        speechCanvas.enabled = false;
+        talkedTo = false;
+    }
 
 	public void GotGift() {
 		gifts++;
 		if (gifts > 3) {
 			curMes = string.Concat("You've given me ", gifts, " flowers! I love you!");
-		}
-	}
+        }
+        talkedTo = true;
+        speech.text = thanks;
+        speechCanvas.enabled = true;
+    }
 
 	void OnTriggerEnter2D(Collider2D other) {
+        talkedTo = true;
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+        talkedTo = false;
 	}
 
 	private void ChangeDirection() {
